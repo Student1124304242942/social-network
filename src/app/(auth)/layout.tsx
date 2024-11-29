28bc29742b84a6785243e1d6a280582f080fcc53
Pulatov Yunus <pulat2007@mail.ru>
2024-11-29 21:16:08 +0500

    changes here again

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,24 @@
+'use client';
+import React, { ReactNode } from 'react';
+import Image from 'next/image';
+ 
+export default function AuthLayout ({ children }: { children: ReactNode }) {
+    return (
+      <div className="min-h-full flex bg-black">
+        <div className="flex-1  relative  bg-black">
+          {children}
+        </div>
+        <div className="flex-1 relative rounded-l-[50px] overflow-hidden z-10 md:flex hidden">
+          <div className="absolute inset-0">
+            <Image
+              src="/auth.svg"
+              alt="auth"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+        </div>
+      </div>
+    );
+}
+ 
