6769b99f4aaafb9075de5d4263797995b654363f
Pulatov Yunus <pulat2007@mail.ru>
2024-12-02 20:56:58 +0500

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: 'dist',
+    experimental: {
+        serverActions: true,
+    },
     images: {
         unoptimized: true,
     },
