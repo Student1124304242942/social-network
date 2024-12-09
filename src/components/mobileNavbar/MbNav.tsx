ca451b835835c8f2b6936ba72bd35edd4dc8c275
Pulatov Yunus <pulat2007@mail.ru>
2024-12-09 22:11:06 +0500

    changes

--- src/components/mobileNavbar/MbNav.tsx
+++ src/components/mobileNavbar/MbNav.tsx
@@ -4,16 +4,15 @@ import LinkElement from '../LinkElement/LinkElement';
  
 // Создаем массив с указанным типом
 export const mbLinks = [
-  { direction: '/profile', icon: 'Profile.svg' },
+  { direction: '/', icon: 'Profile.svg' },
   { direction: '/post', icon: 'Post.svg' },
   { direction: '/users', icon: 'users.svg' },
   { direction: '/messages', icon: 'Chat.svg' },
-  { direction: '/login', icon: 'Exit.svg' },
 ];
 
 const MbNav = () => {
   return (
-    <nav className="flex justify-around p-4 bg-black shadow-lg fixed bottom-0 w-full rounded-t-lg z-10">
+    <nav className="flex justify-around p-4 bg-white shadow-lg fixed bottom-0 w-full rounded-t-lg z-10">
       {mbLinks.map(({ direction, icon }) => (
         <LinkElement key={direction} direction={direction}>
           <div className="text-lg">  <img src={icon} alt={`${direction} icon`} className="h-6 w-6" /></div>
